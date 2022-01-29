7d5b11db9c920b61c01530f77d07f072febdf210
desperu <motais.esteban@laposte.net>
2022-01-29 17:44:53 +0100

    Build Unity Android apk

---
+++ UnityEarth/androidBuild/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,83 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void EarthManager::Start()
+extern void EarthManager_Start_m336158545A601997993FF66B221CD0E58BF3BB21 (void);
+// 0x00000002 System.Void EarthManager::Update()
+extern void EarthManager_Update_m3B590C0C4E983CCB26F1D81F3E2E5043A1E7B305 (void);
+// 0x00000003 System.Void EarthManager::.ctor()
+extern void EarthManager__ctor_m09438DD8C911822E5BB44AA57A125600EC33EE3F (void);
+// 0x00000004 System.Void EarthRotation::Update()
+extern void EarthRotation_Update_m65907604F4C6A1956191C457F8D30C55DF4E72D4 (void);
+// 0x00000005 System.Void EarthRotation::CallFromAndroidWithNoMessage()
+extern void EarthRotation_CallFromAndroidWithNoMessage_m2254984FA09F744276306F90786E2691D4B806BC (void);
+// 0x00000006 System.Void EarthRotation::CallFromAndroidWithMessage(System.String)
+extern void EarthRotation_CallFromAndroidWithMessage_m74FCE0D30F2289204DDE0685496374B096FB900C (void);
+// 0x00000007 System.Void EarthRotation::.ctor()
+extern void EarthRotation__ctor_mE9AE61AAC72419A2C8C938AD8B8C81539F1B2FB8 (void);
+// 0x00000008 System.Void EarthTranslate::Start()
+extern void EarthTranslate_Start_mD0F3811DF8B2EFAF5EFACA97DA57B835B7F30AB5 (void);
+// 0x00000009 System.Void EarthTranslate::Update()
+extern void EarthTranslate_Update_m817F391BE61615B9BE047B04CCD9A62B30768995 (void);
+// 0x0000000A System.Void EarthTranslate::.ctor()
+extern void EarthTranslate__ctor_m5DEA8663032ECCD24962BE1685B1F2541C41A888 (void);
+static Il2CppMethodPointer s_methodPointers[10] = 
+{
+	EarthManager_Start_m336158545A601997993FF66B221CD0E58BF3BB21,
+	EarthManager_Update_m3B590C0C4E983CCB26F1D81F3E2E5043A1E7B305,
+	EarthManager__ctor_m09438DD8C911822E5BB44AA57A125600EC33EE3F,
+	EarthRotation_Update_m65907604F4C6A1956191C457F8D30C55DF4E72D4,
+	EarthRotation_CallFromAndroidWithNoMessage_m2254984FA09F744276306F90786E2691D4B806BC,
+	EarthRotation_CallFromAndroidWithMessage_m74FCE0D30F2289204DDE0685496374B096FB900C,
+	EarthRotation__ctor_mE9AE61AAC72419A2C8C938AD8B8C81539F1B2FB8,
+	EarthTranslate_Start_mD0F3811DF8B2EFAF5EFACA97DA57B835B7F30AB5,
+	EarthTranslate_Update_m817F391BE61615B9BE047B04CCD9A62B30768995,
+	EarthTranslate__ctor_m5DEA8663032ECCD24962BE1685B1F2541C41A888,
+};
+static const int32_t s_InvokerIndices[10] = 
+{
+	856,
+	856,
+	856,
+	856,
+	856,
+	742,
+	856,
+	856,
+	856,
+	856,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	10,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ UnityEarth/androidBuild/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/System.Core_CodeGen.c
@@ -0,0 +1,222 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Exception System.Linq.Error::ArgumentNull(System.String)
+extern void Error_ArgumentNull_m0EDA0D46D72CA692518E3E2EB75B48044D8FD41E (void);
+// 0x00000002 System.Exception System.Linq.Error::MoreThanOneMatch()
+extern void Error_MoreThanOneMatch_m4C4756AF34A76EF12F3B2B6D8C78DE547F0FBCF8 (void);
+// 0x00000003 System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable::Where(System.Collections.Generic.IEnumerable`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x00000004 System.Func`2<TSource,System.Boolean> System.Linq.Enumerable::CombinePredicates(System.Func`2<TSource,System.Boolean>,System.Func`2<TSource,System.Boolean>)
+// 0x00000005 TSource System.Linq.Enumerable::SingleOrDefault(System.Collections.Generic.IEnumerable`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x00000006 System.Boolean System.Linq.Enumerable::Any(System.Collections.Generic.IEnumerable`1<TSource>)
+// 0x00000007 System.Boolean System.Linq.Enumerable::Any(System.Collections.Generic.IEnumerable`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x00000008 System.Boolean System.Linq.Enumerable::Contains(System.Collections.Generic.IEnumerable`1<TSource>,TSource)
+// 0x00000009 System.Boolean System.Linq.Enumerable::Contains(System.Collections.Generic.IEnumerable`1<TSource>,TSource,System.Collections.Generic.IEqualityComparer`1<TSource>)
+// 0x0000000A System.Void System.Linq.Enumerable/Iterator`1::.ctor()
+// 0x0000000B TSource System.Linq.Enumerable/Iterator`1::get_Current()
+// 0x0000000C System.Linq.Enumerable/Iterator`1<TSource> System.Linq.Enumerable/Iterator`1::Clone()
+// 0x0000000D System.Void System.Linq.Enumerable/Iterator`1::Dispose()
+// 0x0000000E System.Collections.Generic.IEnumerator`1<TSource> System.Linq.Enumerable/Iterator`1::GetEnumerator()
+// 0x0000000F System.Boolean System.Linq.Enumerable/Iterator`1::MoveNext()
+// 0x00000010 System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable/Iterator`1::Where(System.Func`2<TSource,System.Boolean>)
+// 0x00000011 System.Object System.Linq.Enumerable/Iterator`1::System.Collections.IEnumerator.get_Current()
+// 0x00000012 System.Collections.IEnumerator System.Linq.Enumerable/Iterator`1::System.Collections.IEnumerable.GetEnumerator()
+// 0x00000013 System.Void System.Linq.Enumerable/WhereEnumerableIterator`1::.ctor(System.Collections.Generic.IEnumerable`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x00000014 System.Linq.Enumerable/Iterator`1<TSource> System.Linq.Enumerable/WhereEnumerableIterator`1::Clone()
+// 0x00000015 System.Void System.Linq.Enumerable/WhereEnumerableIterator`1::Dispose()
+// 0x00000016 System.Boolean System.Linq.Enumerable/WhereEnumerableIterator`1::MoveNext()
+// 0x00000017 System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable/WhereEnumerableIterator`1::Where(System.Func`2<TSource,System.Boolean>)
+// 0x00000018 System.Void System.Linq.Enumerable/WhereArrayIterator`1::.ctor(TSource[],System.Func`2<TSource,System.Boolean>)
+// 0x00000019 System.Linq.Enumerable/Iterator`1<TSource> System.Linq.Enumerable/WhereArrayIterator`1::Clone()
+// 0x0000001A System.Boolean System.Linq.Enumerable/WhereArrayIterator`1::MoveNext()
+// 0x0000001B System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable/WhereArrayIterator`1::Where(System.Func`2<TSource,System.Boolean>)
+// 0x0000001C System.Void System.Linq.Enumerable/WhereListIterator`1::.ctor(System.Collections.Generic.List`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x0000001D System.Linq.Enumerable/Iterator`1<TSource> System.Linq.Enumerable/WhereListIterator`1::Clone()
+// 0x0000001E System.Boolean System.Linq.Enumerable/WhereListIterator`1::MoveNext()
+// 0x0000001F System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable/WhereListIterator`1::Where(System.Func`2<TSource,System.Boolean>)
+// 0x00000020 System.Void System.Linq.Enumerable/<>c__DisplayClass6_0`1::.ctor()
+// 0x00000021 System.Boolean System.Linq.Enumerable/<>c__DisplayClass6_0`1::<CombinePredicates>b__0(TSource)
+static Il2CppMethodPointer s_methodPointers[33] = 
+{
+	Error_ArgumentNull_m0EDA0D46D72CA692518E3E2EB75B48044D8FD41E,
+	Error_MoreThanOneMatch_m4C4756AF34A76EF12F3B2B6D8C78DE547F0FBCF8,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
+static const int32_t s_InvokerIndices[33] = 
+{
+	1443,
+	1503,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+};
+static const Il2CppTokenRangePair s_rgctxIndices[12] = 
+{
+	{ 0x02000004, { 29, 4 } },
+	{ 0x02000005, { 33, 9 } },
+	{ 0x02000006, { 42, 7 } },
+	{ 0x02000007, { 49, 10 } },
+	{ 0x02000008, { 59, 1 } },
+	{ 0x06000003, { 0, 10 } },
+	{ 0x06000004, { 10, 5 } },
+	{ 0x06000005, { 15, 3 } },
+	{ 0x06000006, { 18, 1 } },
+	{ 0x06000007, { 19, 3 } },
+	{ 0x06000008, { 22, 2 } },
+	{ 0x06000009, { 24, 5 } },
+};
+static const Il2CppRGCTXDefinition s_rgctxValues[60] = 
+{
+	{ (Il2CppRGCTXDataType)2, 728 },
+	{ (Il2CppRGCTXDataType)3, 1679 },
+	{ (Il2CppRGCTXDataType)2, 1191 },
+	{ (Il2CppRGCTXDataType)2, 950 },
+	{ (Il2CppRGCTXDataType)3, 2795 },
+	{ (Il2CppRGCTXDataType)2, 758 },
+	{ (Il2CppRGCTXDataType)2, 954 },
+	{ (Il2CppRGCTXDataType)3, 2808 },
+	{ (Il2CppRGCTXDataType)2, 952 },
+	{ (Il2CppRGCTXDataType)3, 2801 },
+	{ (Il2CppRGCTXDataType)2, 297 },
+	{ (Il2CppRGCTXDataType)3, 14 },
+	{ (Il2CppRGCTXDataType)3, 15 },
+	{ (Il2CppRGCTXDataType)2, 483 },
+	{ (Il2CppRGCTXDataType)3, 1269 },
+	{ (Il2CppRGCTXDataType)2, 565 },
+	{ (Il2CppRGCTXDataType)2, 596 },
+	{ (Il2CppRGCTXDataType)3, 1270 },
+	{ (Il2CppRGCTXDataType)2, 560 },
+	{ (Il2CppRGCTXDataType)2, 561 },
+	{ (Il2CppRGCTXDataType)2, 594 },
+	{ (Il2CppRGCTXDataType)3, 1268 },
+	{ (Il2CppRGCTXDataType)2, 509 },
+	{ (Il2CppRGCTXDataType)3, 3561 },
+	{ (Il2CppRGCTXDataType)3, 1115 },
+	{ (Il2CppRGCTXDataType)2, 448 },
+	{ (Il2CppRGCTXDataType)2, 563 },
+	{ (Il2CppRGCTXDataType)2, 595 },
+	{ (Il2CppRGCTXDataType)2, 623 },
+	{ (Il2CppRGCTXDataType)3, 1680 },
+	{ (Il2CppRGCTXDataType)3, 1682 },
+	{ (Il2CppRGCTXDataType)2, 212 },
+	{ (Il2CppRGCTXDataType)3, 1681 },
+	{ (Il2CppRGCTXDataType)3, 1689 },
+	{ (Il2CppRGCTXDataType)2, 731 },
+	{ (Il2CppRGCTXDataType)2, 953 },
+	{ (Il2CppRGCTXDataType)3, 2802 },
+	{ (Il2CppRGCTXDataType)3, 1690 },
+	{ (Il2CppRGCTXDataType)2, 577 },
+	{ (Il2CppRGCTXDataType)2, 607 },
+	{ (Il2CppRGCTXDataType)3, 1274 },
+	{ (Il2CppRGCTXDataType)3, 3556 },
+	{ (Il2CppRGCTXDataType)3, 1683 },
+	{ (Il2CppRGCTXDataType)2, 730 },
+	{ (Il2CppRGCTXDataType)2, 951 },
+	{ (Il2CppRGCTXDataType)3, 2796 },
+	{ (Il2CppRGCTXDataType)3, 1273 },
+	{ (Il2CppRGCTXDataType)3, 1684 },
+	{ (Il2CppRGCTXDataType)3, 3555 },
+	{ (Il2CppRGCTXDataType)3, 1695 },
+	{ (Il2CppRGCTXDataType)2, 732 },
+	{ (Il2CppRGCTXDataType)2, 955 },
+	{ (Il2CppRGCTXDataType)3, 2809 },
+	{ (Il2CppRGCTXDataType)3, 1830 },
+	{ (Il2CppRGCTXDataType)3, 986 },
+	{ (Il2CppRGCTXDataType)3, 1275 },
+	{ (Il2CppRGCTXDataType)3, 985 },
+	{ (Il2CppRGCTXDataType)3, 1696 },
+	{ (Il2CppRGCTXDataType)3, 3557 },
+	{ (Il2CppRGCTXDataType)3, 1272 },
+};
+extern const CustomAttributesCacheGenerator g_System_Core_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_System_Core_CodeGenModule;
+const Il2CppCodeGenModule g_System_Core_CodeGenModule = 
+{
+	"System.Core.dll",
+	33,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	12,
+	s_rgctxIndices,
+	60,
+	s_rgctxValues,
+	NULL,
+	g_System_Core_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
