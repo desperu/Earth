6e78f53168039cc01cb328baf4d84696adc03111
desperu <motais.esteban@laposte.net>
2022-01-29 17:11:23 +0100

    Create scripts to be able to control game object from Android Native Code

---
+++ UnityEarth/Assets/Scripts/EarthRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotation : MonoBehaviour
+{
+
+    private float speed = 40f;
+
+    // Update is called once per frame
+    private void Update() {
+        gameObject.transform.Rotate(0f, speed * Time.deltaTime, 0f);
+    }
+
+    public void CallFromAndroidWithNoMessage() {
+        speed = speed * 2;
+    }
+	
+    public void CallFromAndroidWithMessage(string value) {
+        speed = float.Parse(value);
+        Debug.Log($"Coming from Android: {value}");
+    }
+}
